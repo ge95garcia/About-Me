f49931aa26aef3777e18395eb18f270a11df3d83
Gregory Garcia <gregory@Gregorys-MacBook-Pro-2.local>
2015-02-01 15:44:58 -0600

    First commit of AboutMe

--- About Me/ViewController.m
+++ About Me/ViewController.m
@@ -17,11 +17,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //UIImage *backgroundImage = [UIImage imageNamed:@"mustang"];
+    //UIImageView *imageView = [[UIImageView alloc] initWithImage:backgroundImage];
+    //[self.view addSubview:imageView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Touch Events
+
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+}
+
+- (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+}
+
+- (void) touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSLog(@"touches cancelled");
+}
+
 
 @end
